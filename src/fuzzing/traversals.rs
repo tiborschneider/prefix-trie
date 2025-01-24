1723b18d1746146704311102970e60ed1eda0ae8
Tibor Schneider <sctibor@ethz.ch>
2025-01-24 10:02:51 +0100

    Implement Send and Sync, add test

--- src/fuzzing/traversals.rs
+++ src/fuzzing/traversals.rs
@@ -1,3 +1,10 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use itertools::Itertools;
+
 use super::*;
 
 qc!(children, _children);
@@ -44,4 +51,60 @@ fn _children_values_trieview((map, start): (PrefixMap<TestPrefix, i32>, TestPref
     } else {
         want.is_empty()
     }
+}
+
+qc!(simultaneous_iter_mut, _simultaneous_iter_mut);
+fn _simultaneous_iter_mut(mut map: PrefixMap<TestPrefix, i32>) -> bool {
+    // go to the first split
+    let mut view = map.view_mut();
+    let (left, right) = loop {
+        match view.split() {
+            (None, None) => return true,
+            (None, Some(v)) | (Some(v), None) => view = v,
+            (Some(left), Some(right)) => break (left, right),
+        }
+    };
+
+    let want = left
+        .view()
+        .into_iter()
+        .chain(right.view())
+        .map(|(p, t)| (*p, t.saturating_mul(4)))
+        .sorted()
+        .collect::<Vec<_>>();
+
+    // simultaneously traverse left and right
+    let result = Arc::new(Mutex::new(Vec::new()));
+    let res_left = result.clone();
+    let res_right = result.clone();
+
+    fn foo<'a>(
+        view: TrieViewMut<'a, TestPrefix, i32>,
+        res: Arc<Mutex<Vec<(&'a TestPrefix, &'a mut i32)>>>,
+    ) {
+        let mut references = Vec::new();
+        for (p, t) in view {
+            *t = t.saturating_mul(2);
+            references.push((p, t));
+        }
+        res.lock().unwrap().extend(references);
+    }
+
+    thread::scope(move |s| {
+        let thread_left = s.spawn(move || foo(left, res_left));
+        let thread_right = s.spawn(move || foo(right, res_right));
+        thread_left.join().unwrap();
+        thread_right.join().unwrap();
+    });
+
+    // take the result and multiply all values by 2 again
+    let mut result = result.lock().unwrap();
+    let mut got = Vec::new();
+    for (p, t) in result.iter_mut() {
+        **t = t.saturating_mul(2);
+        got.push((**p, **t));
+    }
+    got.sort();
+
+    want == got
 }
